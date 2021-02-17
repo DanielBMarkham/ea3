b7309ec5468d907495ee007b82cb546662d6c64f
Daniel Markham <danielbmarkham@hotmail.com>
2021-02-17 16:57:20 -0500

    beginning to organize things

--- Parser.fs
+++ Parser.fs
@@ -1,3 +1,22 @@
 module Parser
 
+open System
+open FParsec
 open Types
+
+
+let test p str =
+    match run p str with
+    | Success(result, _, _)   -> printfn "Success: %A" result
+    | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
+
+let n:Parser<string,string> = pstring "dog"
+
+let markdownListIndicator = pstring " - "
+
+let getTokenLeadingLine:Parser<string,unit> =
+  let restOfLine=manyCharsTill anyChar (skipNewline <|> eof)
+  markdownListIndicator .>> restOfLine
+
+let getLineNotAMarkdownList:Parser<string,unit> =
+  manyCharsTill anyChar (skipNewline <|> eof)
--- Types.fs
+++ Types.fs
@@ -2,3 +2,123 @@ module Types
 
 open System
 
+type GenreIdentifier =
+  | World
+  | Business
+  | System
+  | Meta
+type AbstractionLevelIndicator =
+  | Abstract
+  | Realized
+type TemporalIndicator =
+  | Was
+  | AsIs
+  | ToBe
+type BucketIdentifier =
+  | Behavior
+  | Value
+  | Product
+  | Organization
+  type NoteType=
+    | Note
+    | Question
+    | ToDo
+    | Work
+
+ type MinorModelGrouping =
+  | All
+  | WhenBehaviorMinorModelItems
+  | AsAIBehaviorMinorModelItemstems
+  | INeedToBehaviorMinorModelItems
+  | SoThatBehaviorMinorModemItems
+  | ParentBehaviorItems
+  | UsedByStructureItems
+  | AffectedBySupplementalItems
+  | AffectsSupplementalItems
+  | OrganizationHypothesesItems
+  | OrganizationObservationItems
+  | OrganizationExpectedItems
+  | OrganizationExpectedImpactMetrics
+  | OrganizationSuccessfulResultsExpected
+  | OrganizationProposedChangesBehaviorItemList
+  | OrganizationProposedChangesSupplementItemList
+  | OrganizationExpectedExperimentStartDate
+  | OrganizationExpectedExperimentSuspenseTime
+  | OrganizationHypothesisParentItemList
+  | TaskItems
+  | TaskParentItems
+  | TaskUserStoryItems
+  | EntityItems
+  | HasAEntityItems
+  | ContainsEntityItems
+  | UsedByBehaviorItems
+  | BecauseMinorModelItems
+  | WheneverMinorModelItems
+type MinorModelItems =
+  | MinorModelItemList
+  | MindorModelItemsSingleton
+  | All
+type MindorModelLocationIdentifier =
+  | All
+  | Misc
+  | Shortcut
+  | MinorModelGrouping
+type MajorModelItems =
+  | All
+  | MajorModelItemList
+  | MajorModelItemSingelton
+type MajorModelGrouping =
+  | BucketIdentifier
+  | GenreIdentifier
+  | TemporalIndicator
+  | AbstractionLevelIndicator
+  | MajorModelItems
+  | MinorModelLocationIdentifier
+type MajorModelLocationIdentifier =
+  | All
+  | Misc
+  | Shortcut
+  | MajorModelGrouping
+type CompilationCommand =
+  | MajorModelLocationIdentifier
+  | NonIdentifier
+type NonCompilationTag =
+  | CompilationCommand
+  | CodeContent
+type NonLineComment =
+  | CompilationTag
+  | NonCompilationTag
+type NonEmptySpaces =
+  | LineComment
+  | NonLineComment
+type NonBlankLine =
+  | EmptySpaces
+  | NonEmptySpaces
+type NonBlockComment =
+  | BlankLine
+  | NonBlankLine
+type CodeChunkType =
+  | BlockComment
+  | NonBlockComment
+type FileLocation =
+  {
+    LineNumber:int
+    CharacterNumber:int
+  }
+type CodeChunk =
+  {
+    CompilationUnit:string
+    CompilationOrder:int
+    FileName:string
+    BeginLocation:FileLocation
+    EndLocation:FileLocation
+    ChunkType:CodeChunkType
+    Content:string
+    Hash:System.HashCode
+  }
+  /// This is the basic unit of storage.
+  /// Everything else is either a locator or meta
+  type CodeType =
+  | Markdown
+  | MarkdownList
+
